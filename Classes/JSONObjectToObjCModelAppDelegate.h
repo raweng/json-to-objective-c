6ec075383329d295110fc643be04a3a39017f3f6
Akshay Mhatre <akshay.mhatre@raweng.com>
2012-08-02 22:03:06 +0530

    initial commit

---
+++ Classes/JSONObjectToObjCModelAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  JSONObjectToObjCModelAppDelegate.h
+//  JSONObjectToObjCModel
+//
+//  Created by sobin on 4/14/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSONObjectToObjCModelAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
